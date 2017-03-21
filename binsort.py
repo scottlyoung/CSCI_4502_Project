a4caea7a1586c04ba643e207db7e092d9e871fa0
Michael Gilroy <michael.gilroy24@gmail.com>
2017-03-21 13:08:17 -0600

    Added parsing of data at index 1 TODO: use argparse to parse data at user defined index

--- binsort.py
+++ binsort.py
@@ -2,20 +2,40 @@
 
 import numpy as np
 import matplotlib
+import argparse
 
 def parse():
-	f = open("Consumer_Complaints.csv", "r")
+	f = open("complaintNarratives.csv", "r")
 	# first line does not contain relevant data
 	firstLine = f.readline()
+	
+	i = 1
+	names = ['']
+	counts = [0]
 
 	for line in f:
 		# comma delimited
 		lineList = line.split(",")
-		# TODO: strip newline characters and quotes
-		# lineList contains array  of all comma delimited attributes
+		# check for last line
+		if lineList[0] == '\n':
+			continue
+		if len(lineList) > i:
+			if lineList[i] in names:
+				index = names.index(lineList[i])
+				counts[index] = counts[index] + 1
+			else:
+				names.append(lineList[i])
+				counts.append(1)
+	
+	print(names, counts)
+	return names, counts
+			
 
 def main():
 	# argparse?
-	parse()
+	# TODO: argparse to choose attribute
+	parser = argparse.ArgumentParser(description='Parse data by attribute.')
+	parser.add_argument('file', nargs='+', help='filename required')
+	names, counts = parse()
 
 main()
