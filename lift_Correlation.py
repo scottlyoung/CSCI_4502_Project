16a0bed5209a00384d0fbab9d7b11f5fc3291cc6
MIghtyno42 <michael.min@colorado.edu>
2017-03-30 16:24:32 -0600

    Update lift_Correlation.py
    Added comments and made some variable names easier to understand. We can still alter this more to make it print out things other than which type from attribute 2 correlates best with each value in attribute 1.

--- lift_Correlation.py
+++ lift_Correlation.py
@@ -38,6 +38,9 @@ be the same:
 
 
 with open(sys.argv[1], "r") as input_File:
+
+
+	#checks to see if the the two attributes (arguments 2 and 3) are valid.
 	column1 = "None"
 	column2 = "None"  
 	if (sys.argv[2] == "prod" or sys.argv[2] == "issue" or sys.argv[2] == "comp" or sys.argv[2] == "state" or sys.argv[2] == "response"):
@@ -51,15 +54,29 @@ with open(sys.argv[1], "r") as input_File:
 		
 	else:
 		print("\nInvalid Entry for Column 1. Please use the following template: \n\n > python3 Chi_Square_Correlation.py [csv file] [first column] [second column] \n \nWhere [first column] is equal to prod, issue, comp, state, or response.\n")
-	
+
+
+	#If both are valid, we go on
 	if (column1 != "None" and column2 != "None"):
+		
+		
+		#column1_dict holds all the unique values in the first attribute and the number of times they appear in the csv file.
+		#column2_dict does the same.
 		column1_dict = {}
 		column2_dict = {}
+		
+		#combo_dict holds all of the attribute 1 and attribute 2 combinations that appear in the file along with the number of each combination appears.
 		combo_dict = {}
 		
+		#final_dict is for printing which value from attribute 2 is most correlated with attribute 1 at the end. It stores the chi-square value and the most correlated attribute 2 value
 		final_dict = {}
+		
+		#N is the total number of entries
 		N = 0
 		
+		
+		#c1 and c2 correspond to the column numbers that correspond to the two selected attributes
+
 		if column1 == "prod":
 			c1 = 1
 		elif column1 == "issue":
@@ -81,50 +98,69 @@ with open(sys.argv[1], "r") as input_File:
 			c2 = 8
 		elif column2 == "response":
 			c2 = 14
-			
+
+		#This just skips the header line in the csv file
 		next(input_File)
-		
+
+		#converts the csv file to a more parseable list format
 		readcsv = csv.reader(input_File)
 		listcsv = list(readcsv)
-		
+
+		#we iterate through every entry in the csv list	
 		for g in listcsv:
+			
+			#Reads in the entry's value at the first attribute. This records the total count of each unique value
 			if g[c1] in column1_dict:
 				column1_dict[g[c1]] = column1_dict[g[c1]] + 1				
 			else:
 				column1_dict[g[c1]] = 1
+
+			#Do the same for the second attribute
 			if g[c2] in column2_dict:
 				column2_dict[g[c2]] = column2_dict[g[c2]] + 1
 			else:
 				column2_dict[g[c2]] = 1
+				
+				
+			#Do the same for combos (when the first attribute value and the second appear together). Get the total count for each unique combo	
 			tempstr = g[c1] + "|||||" + g[c2]
-
-
+			
 			if tempstr in combo_dict:
 				combo_dict[tempstr][2] = combo_dict[tempstr][2] + 1
 			else:
 				combo_dict[tempstr] = [g[c1],g[c2],1]
+
+
+			#Add 1 to the total entry count
 			N = N + 1
-			final_dict[g[c1]] = ["NULL", -100]
 			
+			#in the final dictionary, store each unique value in column 1. The "NULL" is reserved for the value in the second attribute the correlates best
+			#-100 is for the chi-square value between the chosen attribute 1 and 2 values
+			final_dict[entry[c1]] = ["NULL", -100]
+			
+
+		#For each combination of attribute 1 and 2 values, iterate and:
 		for h in combo_dict:
 
-			keynum = combo_dict[h][0]
-			col2num = combo_dict[h][1]
+			#Get the combination's attribute 1 and attribute 2 values
+			col1value = combo_dict[h][0]
+			col2value = combo_dict[h][1]
 			
-			keyFrac = column1_dict[keynum] / N
-			col2Frac = column2_dict[col2num] / N
+			#Get the ratios of the counts of those values to the total number of entries
+			col1Frac = column1_dict[col1value] / N
+			col2Frac = column2_dict[col2value] / N
 			combinedFrac = combo_dict[h][2] / N
 			
-
-			numb = combinedFrac/(keyFrac * col2Frac)
+			#Calculate correlation coefficient
+			correlation_coefficient = combinedFrac/(col1Frac * col2Frac)
 			
-			if (numb > final_dict[combo_dict[h][0]][1]):
-				final_dict[combo_dict[h][0]] = [col2num, numb]
-				print (col2num,keyFrac, col2Frac, combinedFrac)
-				print("=============")	
+			#if the calculated correlation coefficient is greater than the current value's biggest correlation coefficient, replace it
+			if (correlation_coefficient > final_dict[combo_dict[h][0]][1]):
+				final_dict[combo_dict[h][0]] = [col2value, correlation_coefficient]
+
 	
 
-
+		#Print it all out. We can later change this to print other things (like which two attribute types are negatively correlated)
 		for j in final_dict:
 			print (j, final_dict[j])
 
