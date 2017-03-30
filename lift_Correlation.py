da99b307a34f474ca9899eaa07f779abc3e84cb1
MIghtyno42 <michael.min@colorado.edu>
2017-03-29 18:48:20 -0600

    Preliminary draft of lift correlation
    This code prints out the most correlated object (highest lift) from one attribute and another. It will print out the each element in the first attribute, the element in the second attribute with the highest lift value, and the lift value itself. May have to do a rework on this to see what companies are negatively correlated. May also have to clean this code up later.

---
+++ lift_Correlation.py
@@ -0,0 +1,130 @@
+import sys
+import csv
+
+'''
+Author: Michael Min
+Date: 3/27/2017
+
+This python file is made to take in two columns and determine which values
+in the second column are the best correlated with the values in the first.
+It will run calculations for every unique value in the first column to print the 
+three values from the second column with the highest lift values
+(this will measure which are the best correlated). 
+
+Do be aware, however, that only the nominal attribute columns that have
+non-trivial information will be allowed.
+
+These include the following:
+
+	*Product
+	*Issue
+	*Company
+	*State
+	*Company Response to Consumer
+
+To activate, run the following command:
+
+>python3 lift_Correlation.py [csv file] [first column] [second column] 
+
++first column and second column must be one of the following, but cannot 
+be the same:
+
+	*prod
+	*issue
+	*comp
+	*state
+	*response
+'''
+
+
+with open(sys.argv[1], "r") as input_File:
+	column1 = "None"
+	column2 = "None"  
+	if (sys.argv[2] == "prod" or sys.argv[2] == "issue" or sys.argv[2] == "comp" or sys.argv[2] == "state" or sys.argv[2] == "response"):
+		column1 = sys.argv[2]
+		if (sys.argv[3] == column1):
+			print("\nInvalid entry for column 2. Please make sure that the entries for column 1 and column 2 are different.\n")
+		elif (sys.argv[3] == "prod" or sys.argv[3] == "issue" or sys.argv[3] == "comp" or sys.argv[3] == "state" or sys.argv[3] == "response"):
+			column2 = sys.argv[3]
+		elif (column2 == "None"):
+			print("\nInvalid Entry for Column 2. Please use the following template: \n\n > python3 Chi_Square_Correlation.py [csv file] [first column] [second column] \n \nWhere [second column] is equal to prod, issue, comp, state, or response.\n")
+		
+	else:
+		print("\nInvalid Entry for Column 1. Please use the following template: \n\n > python3 Chi_Square_Correlation.py [csv file] [first column] [second column] \n \nWhere [first column] is equal to prod, issue, comp, state, or response.\n")
+	
+	if (column1 != "None" and column2 != "None"):
+		column1_dict = {}
+		column2_dict = {}
+		combo_dict = {}
+		
+		final_dict = {}
+		N = 0
+		
+		if column1 == "prod":
+			c1 = 1
+		elif column1 == "issue":
+			c1 = 3
+		elif column1 == "comp":
+			c1 = 7
+		elif column1 == "state":
+			c1 = 8
+		elif column1 == "response":
+			c1 = 14
+			
+		if column2 == "prod":
+			c2 = 1
+		elif column2 == "issue":
+			c2 = 3
+		elif column2 == "comp":
+			c2 = 7
+		elif column2 == "state":
+			c2 = 8
+		elif column2 == "response":
+			c2 = 14
+			
+		next(input_File)
+		
+		readcsv = csv.reader(input_File)
+		listcsv = list(readcsv)
+		
+		for g in listcsv:
+			if g[c1] in column1_dict:
+				column1_dict[g[c1]] = column1_dict[g[c1]] + 1				
+			else:
+				column1_dict[g[c1]] = 1
+			if g[c2] in column2_dict:
+				column2_dict[g[c2]] = column2_dict[g[c2]] + 1
+			else:
+				column2_dict[g[c2]] = 1
+			tempstr = g[c1] + "|||||" + g[c2]
+
+
+			if tempstr in combo_dict:
+				combo_dict[tempstr][2] = combo_dict[tempstr][2] + 1
+			else:
+				combo_dict[tempstr] = [g[c1],g[c2],1]
+			N = N + 1
+			final_dict[g[c1]] = ["NULL", -100]
+			
+		for h in combo_dict:
+
+			keynum = combo_dict[h][0]
+			col2num = combo_dict[h][1]
+			
+			keyFrac = column1_dict[keynum] / N
+			col2Frac = column2_dict[col2num] / N
+			combinedFrac = combo_dict[h][2] / N
+			
+
+			numb = combinedFrac/(keyFrac * col2Frac)
+			
+			if (numb > final_dict[combo_dict[h][0]][1]):
+				final_dict[combo_dict[h][0]] = [col2num, numb]
+				print (col2num,keyFrac, col2Frac, combinedFrac)
+				print("=============")	
+	
+
+
+		for j in final_dict:
+			print (j, final_dict[j])
+
